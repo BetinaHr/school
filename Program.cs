2a9d145ec1daf99b7400319a8207b1305351fe95
unknown <profil4e@gmail.com>
2026-02-07 11:46:27 +0200

    message

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter numbers separated by spaces:");
+        string input = Console.ReadLine(); 
+        string[] numbers = input.Split(' ');
+
+        int sum = 0;
+        foreach (string num in numbers)
+        {
+            if (int.TryParse(num, out int n))
+            {
+                sum += n;
+            }
+            else
+            {
+                Console.WriteLine($"'{num}' is not a valid number.");
+            }
+        }
+
+        Console.WriteLine($"The sum of the numbers is: {sum}");
+    }
+}
